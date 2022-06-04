006e31522de2934d61d707c6e617c2a9588da7ec
larissa <oliveir5uwm@gmail.com>
2022-06-03 23:39:52 -0300

    style: adapt font size after normalize

--- src/components/button/button.styled.ts
+++ src/components/button/button.styled.ts
@@ -6,6 +6,7 @@ width: 100%;
 padding: 9px;
 cursor: pointer;
 margin: 0 0 22px;
+font-size: 0.8rem;
 color: ${props => props.theme.palette.typography.primary};
 border-radius: ${props => props.theme.layout.border.medium};
 background-color: ${props => props.theme.palette.core.primary};
