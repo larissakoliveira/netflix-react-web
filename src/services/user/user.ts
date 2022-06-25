bcc6a8367ba81d31c4501ff92721231839f761d0
larissa <oliveir5uwm@gmail.com>
2022-06-25 15:03:16 -0300

    fix: eslint errors

--- src/services/user/user.ts
+++ src/services/user/user.ts
@@ -3,7 +3,7 @@ import { AuthPayload, SignUp } from './user.type';
 
 const userService = () => {
   const auth = (payload: AuthPayload) => axiosInstance.post('/auth', payload);
-  const signUp = (payload: SignUp) => axiosInstance.post('/signUp', payload);
+  const signUp = (payload: SignUp) => axiosInstance.post('/user', payload);
 
   return {
     auth,
