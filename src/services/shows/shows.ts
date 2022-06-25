20ea6f642d31b52073cc43b0badcb4960cfd51c7
larissa <oliveir5uwm@gmail.com>
2022-06-24 23:31:04 -0300

    fix: pass token to showsService as props not using useSelector

--- src/services/shows/shows.ts
+++ src/services/shows/shows.ts
@@ -1,15 +1,14 @@
-import { useSelector } from 'react-redux';
-import { List } from 'store/shows/shows.type';
 import axiosInstance from 'modules/axios/axios';
-import { tokenSelector } from '../../store/user/user.selector';
+import { Props } from './shows.type';
 
-export const showsService = () => {
-  const token = useSelector(tokenSelector);
-  const headers = {
-    Authorization: `Bearer ${token}`,
+export const showsService = ({ token }: Props) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
   };
 
-  const getList = () => axiosInstance.get<List>('/shows', { headers });
+  const getList = () => axiosInstance.get('/shows', config);
 
   return {
     getList,
--- src/services/shows/shows.type.ts
+++ src/services/shows/shows.type.ts
@@ -1,3 +1,7 @@
+export type Props = {
+  token: string
+};
+
 export enum BadRequestErrorMessage {
   BAD_REQUEST = 'An error occurred while trying getting shows',
 }
